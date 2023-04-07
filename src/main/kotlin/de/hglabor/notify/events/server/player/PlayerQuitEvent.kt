7fa6f0762c6da3321fcfd7e69b26a0e7923527b7
mooz <63669478+mooziii@users.noreply.github.com>
2023-04-07 15:28:33 +0200

    Remove unused import

--- src/main/kotlin/de/hglabor/notify/events/server/player/PlayerQuitEvent.kt
+++ src/main/kotlin/de/hglabor/notify/events/server/player/PlayerQuitEvent.kt
@@ -1,6 +1,5 @@
 package de.hglabor.notify.events.server.player
 
-import de.hglabor.notify.mixins.server.MixinPlayerManager
 import me.obsilabor.alert.Event
 import net.fabricmc.api.EnvType
 import net.fabricmc.api.Environment
